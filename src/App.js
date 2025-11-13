74ef9fc7eb13f4aeceddd2ca2aaac53a04d1efa8
Keith Mwaniki Kareithi <95084184+Kreezy09@users.noreply.github.com>
2025-11-13 14:10:18 +0300

    Refactor App component with routing and properties
    Refactor App component to include routing and property fetching.

--- src/App.js
+++ src/App.js
@@ -1,70 +1,52 @@
-import React from "react";
-import { Link } from "react-scroll";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import logo from "../../images/logo/hut.png";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./navbar.css";
-import Dropdown from 'react-bootstrap/Dropdown';
+import { BrowserRouter } from "react-router-dom";
+import AboutUs from "./pages/AboutUs";
+import NavBar from "./components/navbar/Navbar";
+import Footer from "./pages/Footer";
+import Join from "./pages/Join";
+import Loading from "./pages/Header";
+import Properties from "./pages/Properties";
+import "react-scroll";
+import {useEffect, useState} from "react";
 
+function App() {
+    const [properties,setProperties] = useState([])
+    useEffect(() => {
+        getProperties();
+    },[])
+    const getProperties = async () => {
+        try {
+            const data = await fetch(
+                `${process.env.REACT_APP_HF}/api/client/properties`
+            );
+            if (data.status !== 200) {
+                throw Error("Failed to fetch")
+            }
+            const result = await data.json()
+            setProperties(result)
 
-function NavBar() {
-  const handleSignIn = (url) => {
-    if (url) {
-      window.location.href = url;
-    } else {
-      console.error('Sign in URL is undefined');
+        } catch (e) {
+            console.error(e)
+        }
     }
-  };
 
-  return (
-    <Navbar expand="lg" className="py-3">
-      <Container>
-        <Navbar.Brand href="#" className="me-lg-5">
-          <img className="logo" src={logo} alt="logo-01" width={50} height={50} />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0" navbarScroll>
-            <Nav.Link>
-              <Link to="properties" smooth={true} duration={500}>
-                Rent
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="about-us" smooth={true} duration={500}>
-                Manage
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="join" smooth={true} duration={500}>
-                Features
-              </Link>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <div className="d-flex align-items-center order">
-          
-          <Dropdown>
-            <Dropdown.Toggle variant="primary" id="dropdown-basic">
-            Sign In
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item onClick={() => handleSignIn(process.env.REACT_APP_TMS_SIGNIN)}>
-                TMS
-              </Dropdown.Item>
-              <Dropdown.Item onClick={() => handleSignIn(process.env.REACT_APP_CLA_SIGNIN)}>
-                CLA
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-        
-      </Container>
-    </Navbar>
+    return (
+    <BrowserRouter>
+      <NavBar />
+      <section id="loading">
+        <Loading properties={properties}/>
+      </section>
+      <section id="properties">
+        <Properties properties={properties}/>
+      </section>
+      <section id="about-us">
+        <AboutUs />
+      </section>
+      <section id="join">
+        <Join />
+      </section>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
-export default NavBar;
+export default App;
