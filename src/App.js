e4f75e9820e2eaaa842ab06a8ae0714f47c132ec
Kreezy09 <keith.mwaniki@students.jkuat.ac.ke>
2025-11-13 14:29:32 +0300

    Refactor App component by removing unused properties state and loading logic, and updating section IDs for clarity

--- src/App.js
+++ src/App.js
@@ -4,44 +4,19 @@ import NavBar from "./components/navbar/Navbar";
 import Footer from "./pages/Footer";
 import Join from "./pages/Join";
 import Loading from "./pages/Header";
-import Properties from "./pages/Properties";
 import "react-scroll";
-import {useEffect, useState} from "react";
 
 function App() {
-    const [properties,setProperties] = useState([])
-    useEffect(() => {
-        getProperties();
-    },[])
-    const getProperties = async () => {
-        try {
-            const data = await fetch(
-                `${process.env.REACT_APP_HF}/api/client/properties`
-            );
-            if (data.status !== 200) {
-                throw Error("Failed to fetch")
-            }
-            const result = await data.json()
-            setProperties(result)
-
-        } catch (e) {
-            console.error(e)
-        }
-    }
-
-    return (
+  return (
     <BrowserRouter>
       <NavBar />
-      <section id="loading">
-        <Loading properties={properties}/>
+      <section id="hero">
+        <Loading />
       </section>
-      <section id="properties">
-        <Properties properties={properties}/>
-      </section>
-      <section id="about-us">
+      <section id="about">
         <AboutUs />
       </section>
-      <section id="join">
+      <section id="features">
         <Join />
       </section>
       <Footer />
