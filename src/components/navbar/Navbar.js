202c8a18ed07b0c8d4545e75db13c169b33eafdd
Kreezy09 <keith.mwaniki@students.jkuat.ac.ke>
2025-11-13 14:29:21 +0300

    Refactor NavBar component to streamline sign-in functionality and update navigation links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,18 +3,18 @@ import { Link } from "react-scroll";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 import logo from "../../images/logo/hut.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./navbar.css";
-import Dropdown from 'react-bootstrap/Dropdown';
-
 
 function NavBar() {
-  const handleSignIn = (url) => {
+  const handleSignIn = () => {
+    const url = process.env.REACT_APP_TMS_SIGNIN;
     if (url) {
       window.location.href = url;
     } else {
-      console.error('Sign in URL is undefined');
+      console.error("Sign in URL is undefined");
     }
   };
 
@@ -22,46 +22,49 @@ function NavBar() {
     <Navbar expand="lg" className="py-3">
       <Container>
         <Navbar.Brand href="#" className="me-lg-5">
-          <img className="logo" src={logo} alt="logo-01" width={50} height={50} />
+          <img
+            className="logo"
+            src={logo}
+            alt="TMS Logo"
+            width={50}
+            height={50}
+          />
+          <span className="ms-2 fw-bold fs-4">TMS</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="me-auto my-2 my-lg-0" navbarScroll>
             <Nav.Link>
-              <Link to="properties" smooth={true} duration={500}>
-                Rent
+              <Link to="hero" smooth={true} duration={500}>
+                Home
               </Link>
             </Nav.Link>
             <Nav.Link>
-              <Link to="about-us" smooth={true} duration={500}>
-                Manage
+              <Link to="about" smooth={true} duration={500}>
+                About
               </Link>
             </Nav.Link>
             <Nav.Link>
-              <Link to="join" smooth={true} duration={500}>
+              <Link to="features" smooth={true} duration={500}>
                 Features
               </Link>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
-        <div className="d-flex align-items-center order">
-          
-          <Dropdown>
-            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+        <div className="d-flex align-items-center gap-2 order">
+          <Button variant="outline-primary" onClick={handleSignIn}>
             Sign In
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item onClick={() => handleSignIn(process.env.REACT_APP_TMS_SIGNIN)}>
-                TMS
-              </Dropdown.Item>
-              <Dropdown.Item onClick={() => handleSignIn(process.env.REACT_APP_CLA_SIGNIN)}>
-                CLA
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() =>
+              (window.location.href =
+                process.env.REACT_APP_TMS + "/auth/signup")
+            }
+          >
+            Get Started
+          </Button>
         </div>
-        
       </Container>
     </Navbar>
   );
